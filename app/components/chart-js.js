e182892f53157e5b475ac353992b65228573d4ae
bhcmoney <money88@gmail.com>
2017-11-23 22:35:10 -0600

    added chart component, need to make generic

---
+++ app/components/chart-js.js
@@ -0,0 +1,112 @@
+import Component from '@ember/component';
+import Chart from 'npm:chart.js';
+
+export default Component.extend({
+	// var myChart = new Chart(ctx, {...});
+
+	didRender() {
+		debugger;
+		console.log('hello');
+		new Chart(document.getElementById("line-chart"), {
+			type: 'line',
+			data: {
+				labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+				datasets: [{
+					data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+					label: "Africa",
+					borderColor: "#3e95cd",
+					fill: false
+				}, {
+					data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+					label: "Asia",
+					borderColor: "#8e5ea2",
+					fill: false
+				}, {
+					data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+					label: "Europe",
+					borderColor: "#3cba9f",
+					fill: false
+				}, {
+					data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+					label: "Latin America",
+					borderColor: "#e8c3b9",
+					fill: false
+				}, {
+					data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+					label: "North America",
+					borderColor: "#c45850",
+					fill: false
+				}]
+			},
+			options: {
+				title: {
+					display: true,
+					text: 'World population per region (in millions)'
+				}
+			}
+		});
+	},
+
+	// 	config: {
+	// 		type: 'line',
+	// 		data: {
+	// 			datasets: [{
+	// 				label: "Temperature (F)",
+	// 				fill: true,
+	// 				backgroundColor: 'rgb(37,44,53)',
+	// 				borderColor: 'rgb(25, 151, 198)',
+	// 				pointBackgroundColor: 'rgb(25, 151, 198)',
+	// 				pointRadius: 0,
+	// 				data: {},
+	// 				cubicInterpolationMode: 'monotone'
+	// 			}]
+	// 		},
+	// 		options: {
+	// 			responsive: true,
+	// 			title: {
+	// 				display: true,
+	// 				text: 'Evo 5 Gallon Temperature'
+	// 			},
+	// 			legend: {
+	// 				display: false
+	// 			},
+	// 			tooltips: {
+	// 				mode: 'index',
+	// 				intersect: false,
+	// 			},
+	// 			hover: {
+	// 				mode: 'nearest',
+	// 				intersect: true
+	// 			},
+	// 			scales: {
+	// 				xAxes: [{
+	// 					type: 'time',
+	// 					time: {
+	// 						displayFormats: {
+	// 							unit: 'minute'
+	// 						}
+	// 					},
+	// 					display: true,
+	// 					scaleLabel: {
+	// 						display: true,
+	// 						labelString: 'Time'
+	// 					},
+	// 					ticks: {
+	// 						autoSkip: true,
+	// 						maxRotation: 60,
+	// 						minRotation: 90
+	// 					}
+	// 				}],
+	// 				yAxes: [{
+	// 					display: true,
+	// 					scaleLabel: {
+	// 						display: true,
+	// 						labelString: 'Temperature (F)'
+	// 					}
+	// 				}]
+	// 			}
+	// 		}
+	// 	}
+	//
+	// });
+});
