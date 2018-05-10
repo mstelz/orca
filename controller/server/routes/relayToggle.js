abdcb9f061a3cf039208a38833cc9e81e471b014
Mike S <money88@gmail.com>
2018-05-10 21:09:45 +0000

    relayToggling via express get endpoint

--- controller/server/routes/relayToggle.js
+++ controller/server/routes/relayToggle.js
@@ -1,19 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var relay = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+var relay1 = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+var relay2 = new Gpio(17, 'out');
+var relay3 = new Gpio(27, 'out');
+var relay4 = new Gpio(22, 'out');
 
-router.get('/', function(req, res, next) {
-  toggle();
-  res.send('respond with a resource');
-});
+function toggle(num) { 
+  switch(num){
+    case '1':
+      t(relay1);
+      console.log('case1');
+      break;
+    case '2': 
+      t(relay2);
+      console.log('case2');
+      break;
+    case '3':
+      t(relay3);
+      console.log('case3');
+      break;
+    case '4':
+      t(relay4);
+      break;
+    default:
+      t(relay1);
+      t(relay2);
+      t(relay3);
+      t(relay4);
+  }
+}
 
-function toggle() { 
+function t(relay) {
   if (relay.readSync() === 0) { //check the pin state, if the state is 0 (or off)
     relay.writeSync(1); //set pin state to 1 (turn relay on)
   } else {
     relay.writeSync(0); //set pin state to 0 (turn relay off)
   }
 }
+
+router.get('/', function(req, res, next) {
+  toggle(req.query.relayNum);
+  res.send(req.query.relayNum);
+});
 
 module.exports = router;
